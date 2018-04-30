4ec9a2d03f6edc9be3c6a5d8b21dda7d1c51ec5f
ami-GS <1991.daiki@gmail.com>
2018-04-30 11:00:15 +0900

    implement table based solution

--- bit_manipulationm/count_bit.cc
+++ bit_manipulationm/count_bit.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int hamming_weight_naive(uint32_t val) {
     int out = 0;
@@ -29,9 +30,32 @@ int hamming_weight_2(uint32_t val) {
     return ret;
 }
 
+std::vector<int> table;
+
+int hamming_weight_table(uint32_t val) {
+    if (table.size() != 255) {
+        for (int i = 0; i < 255; i++) {
+            int tmp = i;
+            table.push_back(0);
+            while (tmp) {
+                tmp = tmp & (tmp - 1);
+                ++table[i];
+            }
+        }
+    }
+    int out = 0;
+    while (val) {
+        out += table[val & 0xff];
+        val >>= 8;
+    }
+    return out;
+}
+
+
+
 int main() {
     int N = 33;
     for (int i = 0; i < N; ++i) {
-        std::cout << i << ": " << hamming_weight(i) << " " << hamming_weight_2(i) << " " << __builtin_popcount(i) << std::endl; //2
+        std::cout << i << ": " << hamming_weight(i) << " " << hamming_weight_2(i) << " " << __builtin_popcount(i) << " " << hamming_weight_table(i) << std::endl; //2
     }
 }
--- bit_manipulationm/reverse_bit.cc
+++ bit_manipulationm/reverse_bit.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <vector>
+using namespace std;
 
 // Be careful to do indexing (i - 1)
 uint32_t calc(uint32_t num) {
@@ -11,6 +12,31 @@ uint32_t calc(uint32_t num) {
     return ans;
 }
 
+vector<uint8_t> table;
+
+uint32_t calc_table(uint32_t num) {
+    if (table.size() != 255) {
+        for (uint8_t i = 0; i < 255; ++i) {
+            uint8_t tmp = i;
+            table.push_back(0);
+            for (int j = 0; tmp; ++j) {
+                table[i] |= (tmp & 1) << (7-j);
+                tmp >>= 1;
+            }
+        }
+    }
+    uint32_t out = 0;
+    int i = 0;
+    while (num) {
+        out |= table[num & 0xff] << (sizeof(out)-1-(i++)) * 8;
+        num >>= 8;
+    }
+    return out;
+}
+
+
 int main() {
-    printf("%d\n", calc(43261596));
+    for (uint32_t i = 1; i < 123456789; i *= 2) {
+        printf("%u %u\n", calc(i), calc_table(i));
+    }
 }
