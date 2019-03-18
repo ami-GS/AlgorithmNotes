9b086352e4f07ae26b56cc5ace5a6fbe97c99bc6
ami-GS <1991.daiki@gmail.com>
2019-03-17 17:01:06 -0700

    add set version

--- array/two_sum.go
+++ array/two_sum.go
@@ -19,8 +19,23 @@ func calc(arr []int, arrNum int, target int) [][2]int {
 	return out
 }
 
+func calcWithSet(arr []int, target int) [][2]int {
+	sumSet := make(map[int]interface{})
+	var out [][2]int
+
+	for _, val := range arr {
+		sub := target - val
+		if _, ok := sumSet[sub]; ok {
+			out = append(out, [2]int{sub, val})
+		}
+		sumSet[val] = struct{}{}
+	}
+	return out
+}
+
 func main() {
 	array := []int{2, 3, 4, 5, 6, 7, 9, 11, 15}
-	out := calc(array, 9, 9)
+	//out := calc(array, 9, 9)
+	out := calcWithSet(array, 9)
 	fmt.Println(out)
 }
